bccf4b32128555914268479e3dcc34f89aca4ea2
Sumedha <suma@Sumedhas-MacBook-Pro.local>
2019-04-22 01:20:05 +0530

    Added Loading Bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import { getInitialData } from '../utils/api'
 import { receiveTweets } from './tweets'
 import { receiveUsers } from './users'
 import { setAuthedUser } from './authedUser'
+import { showLoading, hideLoading } from 'react-redux-loading'
 
 const AUTHENTED_ID = 'tylermcginnis'
 
 export function handleIntialData() {
     return (dispatch) => {
+        dispatch(showLoading())
         return getInitialData()
                 .then(({ users, tweets }) => {
                     dispatch(receiveUsers(users))
                     dispatch(receiveTweets(tweets))
                     dispatch(setAuthedUser(AUTHENTED_ID))
+                    dispatch(hideLoading())
                 })
     }
 
