633bf435646225cac228e1f5735017412aa84eb8
Sumedha <suma@Sumedhas-MacBook-Pro.local>
2019-04-21 23:48:30 +0530

    Added Sorting tweets according to the timestamp

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -22,6 +22,7 @@ class Dashboard extends Component {
 function mapStateToProps({ tweets }) {
     return {
         tweetsIds : Object.keys(tweets)
+        .sort((a,b) => tweets[b].timestamp - tweets[a].timestamp)
     }
 }
 
